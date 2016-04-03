3b801d208a92b5e88002254349f39c66aa9926bd
Yuta Yamada <cokesboy@gmail.com>
2016-04-03 08:55:36 -0700

    Update to more shorter code

--- zundokokiyoshi.nim
+++ zundokokiyoshi.nim
@@ -9,17 +9,12 @@ iterator zundokoGenerator(): ZunDoko =
           of zun:  echo "ズン"; zun
           of doko: echo "ドコ"; doko
 
-proc kiyoshi() =
-  block done:
-    var song: seq[int] = @[]
-    while true:
-      for z_or_d in zundokoGenerator():
-        song.add(z_or_d.int)
-        if z_or_d == doko:
-          defer: song = @[]
-          if song.sum >= 4:
-            echo "キ ヨ シ!"
-            break done
-
-when isMainModule:
-  kiyoshi()
+var song: seq[int] = @[]
+while true:
+  for z_or_d in zundokoGenerator():
+    song.add(z_or_d.int)
+    if z_or_d == doko:
+      defer: song = @[]
+      if song.sum >= 4:
+        echo "キ ヨ シ!"
+        quit 0
